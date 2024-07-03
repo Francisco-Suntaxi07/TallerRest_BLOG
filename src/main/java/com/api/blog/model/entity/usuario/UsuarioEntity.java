f053ef5a3ca0e844f2a3c0254fd47afbbe5041a6
Francisco Suntaxi <sfsuntaxi1@espe.edu.ec>
2024-07-03 10:05:11 -0500

    Uusario api

--- src/main/java/com/api/blog/model/entity/usuario/UsuarioEntity.java
+++ src/main/java/com/api/blog/model/entity/usuario/UsuarioEntity.java
@@ -1,4 +1,30 @@
 package com.api.blog.model.entity.usuario;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "USUARIO")
 public class UsuarioEntity {
+
+    @Id
+    @Column(name = "IDUSUARIO", length = 8, nullable = false)
+    private String idUsuario;
+
+    @Column(name = "NOMBREUSUARIO", length = 32)
+    private String nombreUsuario;
+
+    @Column(name = "EMAILUSUARIO", length = 32)
+    private String emailUsuario;
+
 }
--- src/main/java/com/api/blog/model/repository/usuario/IUsuarioRepository.java
+++ src/main/java/com/api/blog/model/repository/usuario/IUsuarioRepository.java
@@ -1,4 +1,9 @@
 package com.api.blog.model.repository.usuario;
 
-public interface IUsuarioRepository {
+import com.api.blog.model.entity.usuario.UsuarioEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IUsuarioRepository extends CrudRepository<UsuarioEntity,String> {
 }
