0ab9fd2ce91fe224930d2960a1f752ae0a39b29a
Luis Espinosa <lxespinosa@espe.edu.ec>
2024-07-08 09:34:04 -0500

    post validacion

--- src/main/java/com/api/blog/controller/post/PostRestController.java
+++ src/main/java/com/api/blog/controller/post/PostRestController.java
@@ -1,7 +1,9 @@
 package com.api.blog.controller.post;
 
 import com.api.blog.model.entity.post.PostEntity;
+import com.api.blog.model.entity.usuario.UsuarioEntity;
 import com.api.blog.service.post.IPostService;
+import com.api.blog.service.usuario.UsuarioService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +21,8 @@ public class PostRestController {
     private IPostService postService;
 
     private static final Logger logger = LoggerFactory.getLogger(PostRestController.class);
+    @Autowired
+    private UsuarioService usuarioService;
 
     @GetMapping("/all")
     public ResponseEntity<List<PostEntity>> findAllPost() {
@@ -31,9 +35,15 @@ public class PostRestController {
     }
 
     @PostMapping("/save")
-    public ResponseEntity<?> savePost(@RequestHeader("User-Role") String role, @RequestBody PostEntity post) {
-        if (!"AUTOR".equals(role)) {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("No tienes permiso para agregar nuevos posts.");
+    public ResponseEntity<?> savePost(@RequestBody PostEntity post) {
+        Optional<UsuarioEntity> optionalUsuario = usuarioService.findUserById(post.getIdUsuario());
+        if (optionalUsuario.isPresent()) {
+            UsuarioEntity usuario = optionalUsuario.get();
+            if (!usuario.getRole().equalsIgnoreCase("AUTOR")) {
+                return ResponseEntity.status(HttpStatus.FORBIDDEN).body("No tienes permiso para agregar nuevos posts.");
+            }
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuario no encontrado con ID: " + post.getIdUsuario());
         }
         PostEntity newPost;
         try {
@@ -43,21 +53,29 @@ public class PostRestController {
             logger.error("Error al registrar: " + e.getMessage(), e);
             return ResponseEntity.internalServerError().body("Error al registrar: " + e.getMessage());
         }
+
         return new ResponseEntity<>(newPost, HttpStatus.CREATED);
     }
 
-    @DeleteMapping("/delete/{id}")
-    public ResponseEntity<?> deletePostById(@RequestHeader("User-Role") String role, @PathVariable String id) {
-        if (!"ADMIN".equals(role)) {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("No tienes permiso para eliminar posts.");
-        }
+    @DeleteMapping("/delete/{postId}/{userId}")
+    public ResponseEntity<?> deletePostById(@PathVariable String postId, @PathVariable String userId) {
+        Optional<UsuarioEntity> optionalUsuario = usuarioService.findUserById(userId);
+        if (optionalUsuario.isPresent()) {
+            UsuarioEntity usuario = optionalUsuario.get();
 
+            if (!usuario.getRole().equalsIgnoreCase("ADMIN")) {
+                return ResponseEntity.status(HttpStatus.FORBIDDEN).body("No tienes permiso para eliminar posts.");
+            }
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuario no encontrado con ID: " + userId);
+        }
         try {
-            postService.deletePostById(id);
+            postService.deletePostById(postId);
             return ResponseEntity.ok().build();
         } catch (Exception e) {
-            return ResponseEntity.status(500).body("Error al eliminar.");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error al eliminar el post: " + e.getMessage());
         }
     }
+
 
 }
